78aff4a180626a3f6d90efa26ea9d8e9b46a804f
Cristian C. Gorrin <cristian_gorrin@live.com>
2015-02-28 21:39:19 +0100

    Base for the server

--- Server/Program.cs
+++ Server/Program.cs
@@ -4,12 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Threading;
+
 namespace Server
 {
     class Program
     {
         static void Main(string[] args)
         {
+            bool exit = false;
+
+            var obj = new Server();
+            obj.Start();
+
+            while (!exit)
+            {
+                var input = Console.ReadLine();
+
+                switch (input.ToLower())
+                {
+                    case "exit":
+                        exit = true;
+                        obj.Close();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
         }
     }
 }
---
+++ Server/Settings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+
+namespace Server
+{
+    public class Settings
+    {
+        // Default constant settings values
+        private const string DEFAULT_SERVER_IP = "127.0.0.1";
+        private const int DEFAULT_SERVER_PORT = 12345;
+        private const int DEFAULT_SERVER_SIZE = 20;
+        private const int DEFAULT_SERVER_QUEUE = 10;
+
+        // Settings fields
+        public IPEndPoint ServerIP { set; get; }
+        public int ServerSize { set; get; }
+        public int ServerQueueSize { set; get; }
+
+        public Settings()
+        {
+            // Default Settings
+            this.ServerIP = new IPEndPoint(IPAddress.Parse(DEFAULT_SERVER_IP), DEFAULT_SERVER_PORT);
+            this.ServerSize = DEFAULT_SERVER_SIZE;
+            this.ServerQueueSize = DEFAULT_SERVER_QUEUE;
+        }
+    }
+}
